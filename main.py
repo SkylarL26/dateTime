29a6afeeed24f2b0b6b0557a19911aff6edeecf8
Skylar Li <111327846+SkylarL26@users.noreply.github.com>
2024-10-27 17:23:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 from datetime import datetime
-from modal import (
-    wsgi_app
-)
-
-app = App("dateTime")
 
 current = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 print("Current local date and time:", current)
