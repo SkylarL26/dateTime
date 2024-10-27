b8d29736a1854cb8731737d60ffae4d2987615d9
Skylar Li <111327846+SkylarL26@users.noreply.github.com>
2024-10-27 16:27:25 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,2 +1,6 @@
-current = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-print("Current local date and time:", current)
+def main():
+  current = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+  print("Current local date and time:", current)
+
+if __name__ == "__main__":
+    print_local_date_time()
